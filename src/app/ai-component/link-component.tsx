a860d088449d2f9f5f2d4e9d44e543d5ed130920
kaviru <hapuarachchikaviru@gmail.com>
2024-05-21 21:20:39 +0530

    new ai component added

---
+++ src/app/ai-component/link-component.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { link } from "../schema/link";
+
+export const linkComponent = ({ link }: { link?: link }) => {
+    const [showDescription, setshowDescription] = useState(false);
+    return (
+        <div className="bg-inherit backdrop-blur-md p-4 rounded-md m-4 max-w-prose flex items-center justify-between ">
+            <p className="text-black">{showDescription ? link?.description : link?.title && link?.link}</p>
+            <Button
+                onClick={() => setshowDescription(true)}
+                disabled={showDescription}
+                variant="outline"
+                className="ml-4 border border-gray-300 text-black bg-white hover:bg-gray-100 focus:ring-2 focus:ring-violet-500 rounded-md"
+            >
+                Show fun-fact!
+            </Button>
+        </div>
+    );
+};
